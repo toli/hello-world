a84475cb0848939c1de724215366fea16675c1dd
Toli Kuznets <toli@docker.com>
2016-06-19 16:34:57 -0700

    changes for hello-seattle
    Signed-off-by: Toli Kuznets <toli@docker.com>

--- hello.asm
+++ hello.asm
@@ -40,7 +40,7 @@ phdr:
 ; program header size
 phdrsize equ $ - phdr
 
-; Hello World!/your program here
+; Hello Seattle!/your program here
 _start:
 
 	; sys_write(stdout, message, length)
@@ -62,7 +62,7 @@ _start:
 		db 0x0A
 		db 'To generate this message, Docker took the following steps:', 0x0A
 		db ' 1. The Docker client contacted the Docker daemon.', 0x0A
-		db ' 2. The Docker daemon pulled the "hello-world" image from the Docker Hub.', 0x0A
+		db ' 2. The Docker daemon pulled the "hello-seattle" image from the Docker Hub.', 0x0A
 		db ' 3. The Docker daemon created a new container from that image which runs the', 0x0A
 		db '    executable that produces the output you are currently reading.', 0x0A
 		db ' 4. The Docker daemon streamed that output to the Docker client, which sent it', 0x0A
--- update.sh
+++ update.sh
@@ -4,9 +4,9 @@ set -e
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 set -x
-docker build -f Dockerfile.build -t hello-world:build .
-docker run --rm hello-world:build cat hello > hello
+docker build -f Dockerfile.build -t hello-seattle:build .
+docker run --rm hello-seattle:build cat hello > hello
 chmod +x hello
 ./hello > /dev/null
-docker build -t hello-world:test .
-docker run --rm hello-world:test
+docker build -t hello-seattle:test .
+docker run --rm hello-seattle:test
